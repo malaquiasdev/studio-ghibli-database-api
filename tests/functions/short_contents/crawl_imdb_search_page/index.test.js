18a4373bd50ee7353ed04b00de07fa001aaed80e
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-04-05 13:29:07 -0300

    test: better unit test model

--- tests/functions/short_contents/crawl_imdb_search_page/index.test.js
+++ tests/functions/short_contents/crawl_imdb_search_page/index.test.js
@@ -2,15 +2,9 @@ jest.mock(
   "../../../../src/functions/short_contents/crawl_imdb_search_page/fetch_search_page_data"
 );
 
-jest.mock(
-  "../../../../src/functions/short_contents/crawl_imdb_search_page/parse_data_to_dynamodb_schema"
-);
-
 const imdbSearchPageMock = require("../../../mocks/imdb_search_page");
 const response = require("../../../../src/libs/response");
 const fetchSearchPageData = require("../../../../src/functions/short_contents/crawl_imdb_search_page/fetch_search_page_data");
-const parseDataToDynamodbSchema = require("../../../../src/functions/short_contents/crawl_imdb_search_page/parse_data_to_dynamodb_schema");
-
 const crawlImdbSearchPage = require("../../../../src/functions/short_contents/crawl_imdb_search_page");
 
 let ShortContentsModel = null;
@@ -22,8 +16,6 @@ describe("# Function: crawlImdbSearchPage", () => {
     ShortContentsModel = {
       batchPut: jest.fn().mockReturnValue(true),
     };
-
-    parseDataToDynamodbSchema.mockRestore();
   });
 
   describe("## SpyON", () => {
@@ -32,28 +24,22 @@ describe("# Function: crawlImdbSearchPage", () => {
       expect(fetchSearchPageData).toHaveBeenCalledTimes(1);
     });
 
-    test("## Should call parseDataToDynamodbSchema function once", async () => {
-      await crawlImdbSearchPage(response, ShortContentsModel);
-      expect(parseDataToDynamodbSchema).toHaveBeenCalledTimes(1);
-    });
-
     test("## Should call ShortContentsModel.batchPut function once", async () => {
       await crawlImdbSearchPage(response, ShortContentsModel);
       expect(ShortContentsModel.batchPut).toHaveBeenCalledTimes(1);
     });
   });
 
-  /* test("## Should return a array that contains the studio-ghibli short contents", async () => {
-    const response = await findAll();
-    expect(response).toBeInstanceOf(Array);
-    expect(response[0].id).not.toBe("");
-    expect(response[0].imdbID).not.toBe("");
-    expect(response[0].posterURL).not.toBe("");
-    expect(response[0].title).not.toBe("");
-    expect(response[0].publishYear).not.toBe(0);
-    expect(response[0].ageGroup).not.toBe("");
-    expect(response[0].duration).not.toBe("");
-    expect(response[0].genres).not.toBe([""]);
-    expect(response[0].imdbRating).not.toBe(0);
-  }); */
+  describe("## Smoke", () => {
+    test("### Should return a success response with status code 200, no cache and body with an Array data", async () => {
+      const res = await crawlImdbSearchPage(response, ShortContentsModel);
+      expect(res).toBeInstanceOf(Object);
+      expect(res.body).toBeTruthy();
+      expect(res.body).toBeInstanceOf(Array);
+      expect(res.statusCode).toBe(200);
+      expect(res.headers["Cache-Control"]).toBe(
+        "private, max-age=0, no-cache, no-store, must-revalidate"
+      );
+    });
+  });
 });
