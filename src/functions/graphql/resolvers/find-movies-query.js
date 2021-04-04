bea4b9e9b17046791fb5b256a7e2788b6aae8941
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-04-04 11:41:02 -0300

    remove console.log

--- src/functions/graphql/resolvers/find-movies-query.js
+++ src/functions/graphql/resolvers/find-movies-query.js
@@ -1,5 +1,4 @@
 async function findMoviesQuery(_, { language = 'en-US' }, { dataSources }) {
-  console.log('dataSources', dataSources);
   return dataSources.findMovies({
     tableName: dataSources.config.moviesTableName,
     language,
